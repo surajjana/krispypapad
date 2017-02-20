659f4abb78f7826ed6c9887c592b512f7af3db23
Suraj Kumar Jana <surajjana2@gmail.com>
2017-02-20 10:21:45 +0530

    new commit

--- launch/payment/ccavRequestHandler.php
+++ launch/payment/ccavRequestHandler.php
@@ -26,7 +26,7 @@
 
 ?>
 
-<form method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
 <?php
 echo "<input type=hidden name=encRequest value=$encrypted_data>";
 echo "<input type=hidden name=access_code value=$access_code>";
